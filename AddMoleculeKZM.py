b57f16d629085aee9ba153484c5f5a5db5032bd0
Markus Walther <markus@zahnserver.akclark>
2014-04-01 23:39:45 +0200

    Add molecule script

--- AddMoleculeKZM.py
+++ AddMoleculeKZM.py
@@ -8,8 +8,20 @@ parser.add_argument("-i", "--infile", help="Infile from previous step, xyz forma
 parser.add_argument("-a". "--add_molecule", help="Molecule to add, xyz format")
 parser.add_argument("-o", "--outfile", help="Outfile, xyz format")
 
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-r", "--radius_outer", type="float", help="radius from outmost atom to add new molecule at")
+group.add_argument("-R", "--radius_center", type="float", help="radius from center to add new molecule at")
+
 args = parser.parse_args()
 
 iterin = xyzt.XYZIter(args.infile)
 iteradd = xyzt.XYZIter(args.add_molecule)
+
+inframe = iterin.get_last_frame()
+addframe = iteradd.get_last_frame()
+
+inframe = xyzt.center(inframe)
+addframe = xyzt.center(addframe)
+
+
 
