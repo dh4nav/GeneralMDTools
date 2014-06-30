0c3058664392adab54e606d84cd1959d5d4b6757
Markus Walther <markus.walther@fau.de>
2014-06-30 16:14:30 +0200

    added substitutor for Bi visualization (substitutes the first 6 out of 12 atoms from BI to W)

---
+++ SubstBiW.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+
+ipf = open(sys.argv[1])
+opf = open(sys.argv[2], "a")
+
+counter = 0
+
+for l in ipf:
+    if "BI" in l:
+        counter += 1
+        if counter < 7:
+            opf.write(" ".join(["W "] + l.split()[1:] + ["\n"]))
+        else:
+            opf.write(l)
+            if counter > 11:
+                counter = 0
+    else:
+        opf.write(l)
+
