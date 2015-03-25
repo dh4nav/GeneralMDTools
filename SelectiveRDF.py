00a45986974586f8337bd70ebb18e604e55df4a2
Markus Walther <markus.walther@fau.de>
2015-03-25 01:33:43 +0100

    New script to create RDFs with specific pairs of atoms

---
+++ SelectiveRDF.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import xyzt
+import argparse as ap
+import numpy as np
+
+
+parser = ap.ArgumentParser(description="Generate RDF from pairs")
+parser.add_argument("-i", "--input" , help="Input File, xyz", required=True)
+parser.add_argument("-o", "--output" , help="RDF Output File", default=None, required=True)
+parser.add_argument("-p", "--pairs", help="0-based index pairs for RDF", nargs="+")
+parser.add_argument("-n", "--mindist", help="minimum distance", default=0.0, type=float)
+parser.add_argument("-x", "--maxdist", help="maximum distance", type=float)
+parser.add_argument("-b", "--bins", help="number of histogram bins", type=int, default=100)
+args = parser.parse_args()
+
+xyz_iter =  xyzt.GetXYZIter(args.input)
+
+distances = []
+accumulator = 0.0
+rdf = []
+
+for frame in xyz_iter:
+
+    for n in range(0, len(args.pairs), 2):
+        dist = xyzt.Dist(frame['coordinates'][args.pairs[n]], frame['coordinates'][args.pairs[n+1]], frame['boxvector'])
+
+        if dist < args.mindist:
+            continue
+        if args.maxdist:
+            if dist > args.maxdist:
+                continue
+
+        distances.append(dist)
+
+hist = np.histogram(distances, bins=args.bins)
+
+for n, v in enumerate(hist[0]):
+    accumulator += v
+    rdf.append([hist[1][n]+((hist[1][n+1]-hist[1][n])/2.0), v, accumulator])
+
+print hist
+print rdf
+
+opf = open(args.output, "w")
+
+for l in rdf:
+    opf.write(str(l[0]) + " " + str(l[1]) + " " + str(l[2]) + "\n")
+
+opf.close()
+
+#for n, b in enumerate(hist)
