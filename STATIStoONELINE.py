9f6dfd80c276a32b8d9a3756e3626d566a810b74
Markus Walther <markus.walther@fau.de>
2015-06-23 18:32:11 +0200

    modified to accept restarted partial statis file. TODO: Accept initial files with text in line one instead of a blank line

--- STATIStoONELINE.py
+++ STATIStoONELINE.py
@@ -11,13 +11,26 @@ opf = open(sys.argv[2], "w")
 opv = []
 end = True
 count = 0
+mode = 1
+
+#ipf.read()
+
+#if "ENERGY" in ipf.read():
+#    mode = 0
+
+#ipf.close()
+#ipf = open(sys.argv[1])
+
 for n, l in enumerate(ipf):
     if l.strip() == "":
         pass
-    elif n == 0:
+    if "ENERGY" in l:
         pass
-    elif n == 1:
-        pass
+#    elif n == 0 & mode:
+#        print mode
+#        pass
+#    elif n == 1 & mode:
+#        pass
     elif end == True:
         el = l.strip().split()
         #print el
