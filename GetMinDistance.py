0235e0b07d7601ed25d3b827521906ccecfdc57a
Markus Walther <markus@zahnserver.akclark>
2015-07-05 22:02:40 +0200

    added GetMinDistance.py

---
+++ GetMinDistance.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#*-* coding: utf-8 *-*
+
+import argparse as ap
+import xyzt
+import sys 
+
+parser = ap.ArgumentParser()
+
+parser.add_argument("-i", "--input", help="Input file, xyz format", required=True)
+parser.add_argument("-o", "--output", help="Output file, text format. Contains frame numbers with hits", required=True)
+parser.add_argument("-g1", "--group1", help="Elements in group 1", required=True, nargs="+")
+parser.add_argument("-g2", "--group2", help="Elements in group 2. May overlap with group 1. Defaults to group 1.", nargs="+")
+parser.add_argument("-d", "--distance", help="Distanc threshold", required=True)
+
+args = parser.parse_args()
+
+args.group2 = args.group2 if args.group2 else args.group1
+
+xyz_iter =  xyzt.GetXYZIter(args.input)
+
+print args
+
+opf = open(args.output, "w")
+
+for n, ec in enumerate(xyz_iter):
+    listlen = len([x > 0.1 and x < args.distance for x in xyzt.GetDistsDiff(ec, ec)]) 
+    if listlen:
+        opf.write(str(n) + " " + str(listlen) + "\n")
+
+
+
