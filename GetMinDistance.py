71545d89b44ee08799a09369b4c94db0071f75db
Markus Walther <markus@zahnserver.akclark>
2015-07-05 22:34:50 +0200

    bugfixes

--- GetMinDistance.py
+++ GetMinDistance.py
@@ -4,14 +4,14 @@
 import argparse as ap
 import xyzt
 import sys 
-
+import numpy as np
 parser = ap.ArgumentParser()
 
 parser.add_argument("-i", "--input", help="Input file, xyz format", required=True)
 parser.add_argument("-o", "--output", help="Output file, text format. Contains frame numbers with hits", required=True)
 parser.add_argument("-g1", "--group1", help="Elements in group 1", required=True, nargs="+")
 parser.add_argument("-g2", "--group2", help="Elements in group 2. May overlap with group 1. Defaults to group 1.", nargs="+")
-parser.add_argument("-d", "--distance", help="Distanc threshold", required=True)
+parser.add_argument("-d", "--distance", type=float, help="Distanc threshold", required=True)
 
 args = parser.parse_args()
 
@@ -24,9 +24,14 @@ print args
 opf = open(args.output, "w")
 
 for n, ec in enumerate(xyz_iter):
-    listlen = len([x > 0.1 and x < args.distance for x in xyzt.GetDistsDiff(ec, ec)]) 
-    if listlen:
-        opf.write(str(n) + " " + str(listlen) + "\n")
+    distlist= np.ndarray.flatten(xyzt.GetDistsDiff(ec, ec))
+#    print distlist
+    distlist = distlist[distlist > 0.1]
+#    print distlist
+    distlist = distlist[distlist < args.distance]
+#    print distlist
+    if len(distlist):
+        opf.write(str(n) + " " + str(len(distlist)/2) + "\n")
 
 
 
