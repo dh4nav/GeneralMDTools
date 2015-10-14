10f3b75b8000348995d41c3cb52e74c072109611
dh4nav <markus.walther@fau.de>
2015-10-14 18:10:09 +0200

    added doctrings

--- xyzt.py
+++ xyzt.py
@@ -3,13 +3,22 @@ import scipy.cluster.hierarchy as sch
 import scipy.spatial.distance as ssd
 #import unittest as ut
 
+
 class GetXYZIter:
+    """XYZ Iterator class 
+
+    Frame format specification:
+        {'elements':[str, ...], 'coordinates':ndarray([[x,y,z],...]), 'boxvector':float, 'framenumber':int, 'filename':str}"""
+
     def __init__(self, filename):
+        """Return XYZIter object from XYZ-file filename"""
+
         self.filehandle = open(filename)
         self.filename = filename
         self.current = 0
 
     def reader(self):
+        """Return next line from file, or StopIteration on EOF"""
         line = self.filehandle.readline()
         if len(line) == 0:
             raise StopIteration
@@ -17,15 +26,20 @@ class GetXYZIter:
             return line
 
     def __iter__(self):
+        """Return an iterator"""
         return self
 
     def get_last_frame(self):
+        """Return the last-read frame"""
         frame = []
         for frame in self:
             pass
         return frame
 
     def next(self):
+        """Return frame data dictionary of next frame,
+                Raise *ValueError* if no float in comment line (boxsize),
+                Raise *StopIteration* on EOF"""
         self.current += 1
         self.framelength = int(self.reader().strip()) #filehandle.readline().strip())
         line2 = self.reader().strip()
@@ -34,8 +48,6 @@ class GetXYZIter:
         except ValueError:
             self.boxsize = None
 
-#    if args.boxsize != 0.0:
-#        read_boxsize = args.boxsize
         elements = []
         coordinates = []
 
@@ -47,6 +59,7 @@ class GetXYZIter:
         return {'elements':elements, 'coordinates':np.array(coordinates), 'boxvector':self.boxsize, 'framenumber':self.current, 'filename':self.filename}
 
     def __getitem__(self, index):
+        """Return frame with index"""
         newiter = GetXYZIter(self.filename)
 
         for n in range(index):
@@ -55,10 +68,11 @@ class GetXYZIter:
 
 
 def get_extreme_value(vectors):
-    #return vectors.flatten()
+    """Return maximum of absolute of flattened vectors"""
     return np.absolute(vectors.flatten()).max()
 
 def rotate_single_vector_around_origin(vector, angle_x, angle_y, angle_z):
+    """Return vector rotated around origin by 3 angles"""
     return np.array([
         (vector[0] * np.cos(angle_y) * np.cos(angle_z))
             + (vector[1] * ( (np.cos(angle_x) * np.sin(angle_z) ) + ( np.sin(angle_x) * np.sin(angle_y) * np.cos(angle_z) ) ))
@@ -73,14 +87,18 @@ def rotate_single_vector_around_origin(vector, angle_x, angle_y, angle_z):
             + (vector[2] * np.cos(angle_x) * np.cos(angle_y))])
 
 def rotate_around_origin(ec, angle_x, angle_y, angle_z):
+    """Return all vectors in frame rotated around origin by 3 vectors"""
     ec['coordinates'] = rotate_single_vector_around_origin(ec['coordinates'], angle_x * (np.pi / 180.0), angle_y * (np.pi / 180.0), angle_z* (np.pi / 180.0))
     return ec
 
 def Filter(ec, remove=None, keep=None):
+    """**DEPRECATED** Alias method for filter"""
 #    print "F"
     filter(ec, remove, keep)
 
 def filter(ec, remove=None, keep=None):
+    """Return frame dict with elements in remove removed, or with just the elements in keep.
+        Return may be empty"""
     #print ec
     #print remove
     #print keep
@@ -108,12 +126,14 @@ def filter(ec, remove=None, keep=None):
 
 
 def Dist(x0, x1, boxvect):
+    """Return the minimum image convention distance between x0 and x1 with boxvector[0] as box diameter"""
     delta = np.absolute(np.subtract(x0,x1))
     delta = np.where(delta > 0.5 * boxvect[0], boxvect[0] - delta, delta)
     return np.sqrt((delta ** 2).sum(axis=-1))
 
 
 def DistDist(x0, x1, boxvect):
+    """Return the minimum image convention distances for all pairs x0[n], x1[n] with boxvector[0] as box diameter"""
     distvect = []
     for n, a in enumerate(x0):
         if n < (len(x1)-1):
@@ -121,15 +141,19 @@ def DistDist(x0, x1, boxvect):
     return np.array(distvect)
 
 def GetDistSame(ec):
+    """Return the distance matrix between all atoms in the supplied frame"""
     return ssd.pdist(ec['coordinates'])
 
 def GetDistsDiff(ec1, ec2):
+    """Return the distance matrix between all atoms in frame ec1 and all atoms in frame ec2"""
     return ssd.cdist(ec1['coordinates'],ec2['coordinates'])
 
 def Center(ec):
+    """**DEPRECATED** Alias method for center"""
     center(ec)
 
 def center(ec):
+    """Vector of frame geometric center"""
     summator = np.array([0.0,0.0,0.0])
     for c in ec['coordinates']:
         summator = summator + c
@@ -141,16 +165,20 @@ def center(ec):
     return ec
 
 def Move(ec, vect):
+    """**DEPRECATED** Alias method for move"""
     move(ec, vect)
 
 def move(ec, vect):
+    """Return frame with all atoms moved my vector vect"""
     ec['coordinates'] = np.array(ec['coordinates'] + np.array(vect))
     return ec
 
 def Write_XYZ(ec, filename, append=True):
+    """**DEPRECATED** Alias method for write"""
     write(ec, filename, append=True)
 
 def write(ec, filename, append=True):
+    """Write frame to xyz file"""
     #print ec
     print ".",
     if type(ec) == dict:
@@ -172,6 +200,7 @@ def write(ec, filename, append=True):
     opf.close()
 
 def split(ec, start, end):
+    """Return index slice of frame from start to end"""
     ecout = ec.copy()
 
     ecout['elements'] = ec['elements'][start:end]
@@ -180,6 +209,7 @@ def split(ec, start, end):
     return ecout
 
 def merge(eclist):
+    """Return all frames in list eclist merged, in order. Use largest box vector"""
     ecout = eclist[0].copy()
     ecout['coordinates'] = eclist[0]['coordinates'].tolist()
 
@@ -198,6 +228,7 @@ def merge(eclist):
     return ecout
 
 def debox_coordinate(ref, val, box):
+    """Return minum image convention distance between ref and var with box size *box*"""
     if (ref - val) > (box/2.0):
         return (val + box)
     elif (ref - val) < ((-1.0) * (box/2.0)):
@@ -206,6 +237,7 @@ def debox_coordinate(ref, val, box):
         return val
 
 def get_center_of_mass(ec):
+    """Return center of mass of frame"""
     summator = [0.0,0.0,0.0]
 
     for c in ec['coordinates']:
@@ -220,6 +252,7 @@ def get_center_of_mass(ec):
     return summator
 
 def debox_intramolecule(ec, box):
+    """Return frame with atoms placed at minimum image convention positions, with first atom used as reference, for box diameter *box*"""
     ecout = ec.copy()
     newcoordinates = [ec['coordinates'][0]]
 
@@ -230,7 +263,7 @@ def debox_intramolecule(ec, box):
     return ecout
 
 def debox_intermolecule(eclist, box, center_on=0):
-
+    """Return frame with atoms placed at minimum image convention positions, with atom *center_on* used as reference, for box diameter *box*"""
     if center_on < 0:
         center_on = len(eclist['coordinates']) + center_on
 
