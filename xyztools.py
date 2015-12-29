6f100787f9984aa66e82eef873f860478809905f
t <t@me.me>
2015-12-29 13:51:38 +0100

    implemented DLP2HReader

--- xyztools.py
+++ xyztools.py
@@ -56,7 +56,7 @@ class Reader(object):
         # implement get frame length here
         return Null
 
-    def _get_frame(self, seek=None, frame_length=None, marker=None):
+    def _get_frame(self, seek=None, frame_length=None, marker=None, frame_number=None):
 
         #seek if specified
         if seek != None:
@@ -92,17 +92,17 @@ class Reader(object):
 
         #frame known and positive
         if framenum in frameindex:
-            return self.get_frame(seek=self.frameindex[framenum], frame_length=self.framelength)
+            return self.get_frame(seek=self.frameindex[framenum], frame_length=self.framelength, frame_number=framenum)
 
         #frame unknown and positive
         elif framenum > -1:
             for i in xrange(framenum+1):
                 if i == framenum:
-                    return self.get_frame(seek=self.frameindex[framenum], frame_length=self.framelength)
+                    return self.get_frame(seek=self.frameindex[framenum], frame_length=self.framelength, frame_number=framenum)
                 elif i in self.frameindex:
                     pass
                 else:
-                    self.frameindex[i+1] = self._get_next_frame_start(seek=self.frameindex[i], frame_length = self.framelength)
+                    self.frameindex[i+1] = self._get_next_frame_start(seek=self.frameindex[i], frame_length = self.framelength, frame_number=framenum)
 
         #frame negative
         elif framenum < 0:
@@ -165,79 +165,79 @@ class XYZReader(Reader):
 
         return ensemble
 
-class DLP2HReader(object):
+class DLP2HReader(Reader):
 
     def __init__(self, fileobj=None):
-        if type(fileobj) == str:
-            self.filehandle = open(fileobj)
-        elif type(fileobj) == file:
-            self.filehandle = fileobj
+        super.__init__(fileobj)
+        _read_preamble()
 
-        self.framepos = 0
-        self.intraframepos = 0
-        self.framelength = 0
-        self.frameindex = {"0":0}
+    def _read_preamble(self):
+        self.filehandle.seek(0)
+        self.header = self.filehandle.readline().strip()
 
-    def __getitem__(self, framenum=None):
-        print "fn" + str(framenum)
-        if framenum != None:
-            print self.frameindex
-            if framenum != self.framepos:
-                if self.frameindex:
-                    try:
-                        self.filehandle.seek(self.frameindex[str(framenum)])
-                    except:
-                        if self.framepos > framenum:
-                            self.filehandle.seek(0)
-                            self.framepos = 0
-                            self.intraframepos = 0
+        elements = self.filehandle.readline().strip().split()
+        self.trajectory_key = int(elements[0])
+        self.periodic_key = int(elements[1])
+        self.number_atoms = int(elements[2])
 
-                        if self.intraframepos:
-                            while self.intraframepos < self.framelength:
-                                self.filehandle.readline()
-                                self.intraframepos += 1
-                            self.intraframepos = 0
-                            self.framepos += 1
+        self.frameindex[0] = self.filehandle.tell()
 
-                        self.frameindex[str(self.framepos)] = self.fileindex.tell()
+        self.framelength = (self.trajectory_key+2) * self.number_atoms
 
-                        while self.framepos < framenum:
-                            self.framelength = int(self.filehandle().strip()) +2
-                            self.intraframepos += 1
-                            while self.intraframepos < self.framelength:
-                                self.filehandle.readline()
-                                self.intraframepos += 1
-                            self.intraframepos = 0
-                            self.framepos += 1
-                            self.frameindex[str(self.framepos)] = self.fileindex.tell()
+        if self.periodic_key:
+            self.framelength += 4
+        else:
+            self.framelength += 1
 
-        ensemble = ae.AtomEnsemble()
-        ensemble.filename = self.filehandle.name
-        self.framelength = int(self.filehandle.readline().strip())+2
-        ensemble.boxvector = float(self.filehandle.readline().strip())
-        ensemble.framenumber = self.framepos
-        self.intraframepos += 2
+    def _get_frame_length(self):
+        # implement get frame length here
+        if self.framelength == 0:
+            self._read_preamble()
+        return self.framelength
 
-        while self.intraframepos < self.framelength:
+    def _get_frame(self, seek=None, frame_length=None, marker=None, frame_number=None):
+
+        #seek if specified
+        if seek != None:
+            self.filehandle.seek(seek)
+
+        #read frame_length lines if specified and return position
+        if frame_length != None:
             atomproperties = dict()
-            elements = self.filehandle.readline().strip().split()
-            atomproperties['element'] = elements[0]
-            atomproperties['coordinate'] = [float(elements[1]), float(elements[2]), float(elements[3])]
-            if len(elements) > 4:
-                atomproperties['mass'] = float(elements[4])
-            if len(elements) > 5:
-                atomproperties['charge'] = float(elements[5])
-            if len(elements) > 8:
-                atomproperties['velocity'] = [float(elements[6]), float(elements[7]), float(elements[8])]
-            if len(elements) > 11:
-                atomproperties['force'] = [float(elements[9]), float(elements[10]), float(elements[11])]
-            if len(elements) > 12:
-                atomproperties['molecule_index'] = int(elements[12])
+            ensemble = ae.AtomEnsemble()
 
-            ensemble.append(ae.Atom(**atomproperties))
-            self.intraframepos += 1
+            ensemble.filename = self.filehandle.name
+            ensemble.framenumber = frame_number
 
-        self.intraframepos = 0
-        self.framepos += 1
-        self.frameindex[str(self.framepos)] = self.filehandle.tell()
-        return ensemble
+            atoms_start = 1
+            if self.periodic_key > 0:
+                atoms_start += 3
+            atom_length = self.trajectory_key + 2
+            atomproperties = dict()
+
+            for i in xrange(frame_length):
+                elements = self.filehandle.readline().strip().split()
+                atom_position = (i - atom_start)%atom_length
+                if i == 0:
+                    ensemble.timestep = int(elements[1])
+                    ensemble.time = float(elements[5])
+                    if elements[0].strip() is not "timestep":
+                        raise SyntaxError("Word timestep not found. Frame:" + str(frame_number) + ", Line:" + str(i))
+                elif self.periodic_key and (i == 1):
+                    ensemble.boxvector = float(elements[0])
+                elif atom_position == 0:
+                    atomproperties['element'] = elements[0]
+                    atomproperties['mass'] = float(elements[2])
+                    atomproperties['charge'] = float(elements[3])
+
+                elif atom_position == 1:
+                    atomproperties['velocity'] = [float(elements[0]), float(elements[1]), float(elements[2])]
+
+                elif atom_position == 2:
+                    atomproperties['force'] = [float(elements[0]), float(elements[1]), float(elements[2])]
+
+                if atom_position == (atom_length-1):
+                    ensemble.append(ae.Atom(**atomproperties))
+                    atomproperties = dict()
+
+            return ensemble
