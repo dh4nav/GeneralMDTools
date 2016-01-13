062781b38e272b9fb079ec88afdf2f13a1240092
Markus Walther <markus.walther@fau.de>
2016-01-13 16:03:03 +0100

    useful functions written :)

--- automationtools.py
+++ automationtools.py
@@ -1,17 +1,41 @@
 import os
 import datetime
+#import shutil as su
 
-def check_files_present(filenames, error_message=None):
+def move_and_timestamp_file(filenames, postfix=None, add_date=True, separator="_"):
+
+    if type(filenames) != list:
+        filenames = [filenames]
+
+    for fn in filenames:
+        splitpath = os.path.split(fn)
+        if len(splitpath[1]) == 0:
+            raise OSError
+        else:
+            if postfix != None:
+                splitpath[1] = splitpath[1] + separator + postfix
+            if add_date == True:
+                splitpath[1] = splitpath[1] + separator + datetime.datetime.isoformat(datetime.datetime.now())
+
+            os.rename(fn, os.path.join(splitpath[0], splitpath[1]))
+
+
+
+def check_files_present(filenames):
+
     if type(filenames) != list:
         filenames = [filenames]
 
     for f in filenames:
         if os.path.isfile(f) == False:
-            with open("STOP", "a") as of:
-                of.write(datetime.datetime.isoformat(datetime.datetime.now()))
-                if error_message != None:
-                    of.write(" : " + error_message + "\n")
-                else:
-                    of.write("\n")
+            raise OSError
 
-                    
+def write_error_message(filename, error_message=None):
+    with open(filename, "a") as of:
+        of.write(datetime.datetime.isoformat(datetime.datetime.now()))
+        if error_message != None:
+            of.write(" : " + error_message + "\n")
+        else:
+            of.write("\n")
+
+#def check_and_copy(filenames):
